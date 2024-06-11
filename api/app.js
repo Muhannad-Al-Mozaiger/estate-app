e2fd8ad4916e0768149b26c3c62e9bc5d7391304
Muhannad Al-Mozaiger <programmer22muhannad@gmail.com>
2024-06-11 23:31:48 +0300

    login logout and middleware

--- api/app.js
+++ api/app.js
@@ -3,13 +3,17 @@ import cookieParser from "cookie-parser";
 import authRoute from "./routes/auth.route.js";
 import postRoute from "./routes/post.route.js";
 import userRoute from "./routes/user.route.js";
-
+import testRoute from "./routes/test.route.js";
+import cors from "cors";
 const app = express();
+app.use(cors({origin:process.env.CLIENT_URL, credentials:true}));
 app.use(express.json());
 app.use(cookieParser());
+
 app.use("/api/auth", authRoute);
 app.use("/api/post", postRoute);    
 app.use("/api/user", userRoute);
+app.use("/api/test", testRoute);
 app.listen(3000, () => {
 
     console.log("Server running on port 3000");
