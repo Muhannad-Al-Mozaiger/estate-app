7d3a59905b5d4feb3fb260e4b986c4992bbf3dda
Muhannad Al-Mozaiger <programmer22muhannad@gmail.com>
2024-06-28 01:50:12 +0300

    React Fetch Data Based on URL query

--- client/src/lib/loaders.js
+++ client/src/lib/loaders.js
@@ -1,6 +1,15 @@
-import  apiRequest  from "./apiRequest";
+import apiRequest from "./apiRequest";
 export const singlePageLoader = async ({ request, params }) => {
     const res = await apiRequest(`/posts/${params.id}`);
-    return   res.data ;
-    
+    return res.data;
+}
+export const listPageLoader = async ({ request, params }) => {
+    try {
+        const query = request.url.split('?')[1];
+        const res = await apiRequest(`/posts?${query}`);
+        return res.data;
+    } catch (error) {
+        console.log(error);
+    }
+
 }
